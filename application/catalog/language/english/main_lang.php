dd78c6ebe68f7b86a78c83b4cddf6bcf798bcc46
panigrc <panigrc@hotmail.com>
2018-07-14 10:27:19 +0200

    Updated to CodeIgniter 3.1.9 Language directory moved to Application Directory Application Directory moved to root Application config updated, errors moved to views and missing directories added. index.php and admin.php adapted. Converted Files to LF format

---
+++ application/catalog/language/english/main_lang.php
@@ -0,0 +1,137 @@
+<?php
+    $lang['main_username']		= 'Username';
+    $lang['main_password']		= 'Password';
+    
+    $lang['main_code']		= 'Identifier';
+    $lang['main_category']		= 'Category';
+    $lang['main_categories']		= 'Categories';
+    $lang['main_all']		= 'Όλα';
+    
+    $lang['main_language']		= 'Language';
+    $lang['main_greek']		= 'Greek';
+    $lang['main_german']		= 'German';
+    $lang['main_english']		= 'English';
+    
+    // product
+    
+    $lang['main_title']		= 'Title';
+    $lang['main_description']		= 'Description';
+    $lang['main_published']		= 'Published';
+    $lang['main_image']		= 'Image';
+    $lang['main_url']		= 'url';
+    $lang['main_stock']		= 'Stock';
+    $lang['main_stock_available']		= 'Available';
+    $lang['main_stock_short']		= 'Runs out';
+    $lang['main_main_image']		= 'Main image';
+    $lang['main_price_old']		= 'Price before';
+    
+    // admin
+    
+    $lang['main_save']		= 'Αποθήκευση';
+    $lang['main_cancel']		= 'Ακύρωση';
+    $lang['main_delete']		= 'Διαγραφή';
+    $lang['main_edit']		= 'Επεξεργασία';
+    $lang['main_add_images']		= 'Προσθήκη Εικόνων';
+    
+    // navigation bar
+    
+    $lang['main_home']		= 'Home';
+    $lang['main_catalog']		= 'Product Catalog';
+    $lang['main_news']		= 'News';
+    $lang['main_contact']		= 'Contact';
+    
+    // site info
+    
+    $lang['main_meta_description']		= 'Best Posters here';
+    $lang['main_meta_keywords']		= 'posters, movies, drama';
+    
+    $lang['main_slogan']		= '';
+    
+    // search
+    
+    $lang['main_from']		= 'From';
+    $lang['main_to']		= 'To';
+    $lang['main_order_by']		= 'Order by';
+    $lang['main_search']		= 'Search';
+    
+    $lang['main_priceasc']		= 'Price (Asc)';
+    $lang['main_pricedesc']		= 'Price (Desc)';
+    $lang['main_dateasc']		= 'Date (Asc)';
+    $lang['main_datedesc']		= 'Date (Desc)';
+    
+    $lang['main_page']		= 'Page';
+    $lang['main_page_first']		= 'First';
+    $lang['main_page_last']		= 'Last';
+    $lang['main_go_back']		= 'Go Back';
+    
+    $lang['main_select_language']		= 'Select Language';
+    $lang['main_random_product']		= 'Tip';
+    $lang['main_information']		= 'Information';
+    $lang['main_friendly_sites']		= 'Friendly Sites';
+    
+    $lang['main_price']		= 'Price';
+    $lang['main_currency']		= '€';
+    $lang['main_price_decleration']		= '(Prices include VAT 19%)';
+    
+    // cart
+    
+    $lang['main_cart_update']		= 'Updating cart ...';
+    $lang['main_cart_cost']		= 'Subtotal';
+    $lang['main_checkout']		= 'Checkout';
+    $lang['main_cart']		= 'Your cart';
+    $lang['main_cart_add']		= 'Add to cart';
+    
+    // checkout
+    
+    $lang['main_optional_data']		= 'Optional';
+    $lang['main_required_field']		= 'This field is required to continue your order';
+    $lang['main_load_data']		= 'If you have ordered in the past from us you can fill this form to load your personal data';
+    $lang['main_first_time_user']		= 'If you order for the first time please fill the following form';
+    $lang['main_phone_or_email']		= 'Phone or e-mail';
+    
+    // contact and user information
+    
+    $lang['main_full_name']		= 'Full Name';
+    $lang['main_name']		= 'Name';
+    $lang['main_surname']		= 'Surname';
+    $lang['main_email']		= 'Email';
+    $lang['main_birthdate']		= 'Birthday';
+    $lang['main_address']		= 'Address';
+    $lang['main_city']		= 'City';
+    $lang['main_zip']		= 'Zip';
+    $lang['main_country']		= 'Country';
+    $lang['main_phone']		= 'Phone';
+    $lang['main_company']		= 'Company';
+    $lang['main_website']		= 'Website';
+    $lang['main_message']		= 'Message';
+    $lang['main_message_sent']		= 'Your message has been sent successfully. Thank you for contacting us. We will reply you soon.';
+    $lang['main_message_not_sent']		= 'Your message has not been sent, we apologize. Please try again.';
+    $lang['main_submit']		= 'Submit';
+    $lang['main_spam_protection']		= 'Select the second';
+    $lang['main_spam']		= 'This is spam';
+    $lang['main_not_spam']		= 'This is not spam';
+    
+    // shippment
+    
+    $lang['main_shippment']		= 'Shippment';
+    $lang['main_shippment_sum']		= 'Shippment costs summary';
+    $lang['main_shippment_express']		= 'Express';
+    $lang['main_shippment_to_door']		= 'At your door';
+    $lang['main_shippment_cash_on_delivery']		= 'Cash on delivery';
+    
+    $lang['main_shippment_costs']		= '45';
+    $lang['main_shippment_express_costs']		= '23';
+    $lang['main_shippment_to_door_costs']		= '23';
+    $lang['main_shippment_cash_on_delivery_costs']		= '23';
+        
+    // costs
+    
+    $lang['main_costs_sum']		= 'Sum';
+    
+    // questionnaire
+    
+    $lang['main_questionnaire']		= 'Questionnaire';
+    $lang['main_question1']		= 'Τί άλλο να φέρουμε';
+    $lang['main_question2']		= 'Απο που μας βρήκατε';
+    
+?>
---
+++ application/catalog/language/german/main_lang.php
@@ -0,0 +1,137 @@
+<?php
+    $lang['main_username']		= 'Username';
+    $lang['main_password']		= 'Kennwort';
+    
+    $lang['main_code']		= ' Bezeichner';
+    $lang['main_category']		= 'Kategorie';
+    $lang['main_category']		= 'Kategorien';
+    $lang['main_all']		= 'Alle';
+    
+    $lang['main_language']		= 'Sprache';
+    $lang['main_greek']		= 'Griechisch';
+    $lang['main_german']		= 'Deutsch';
+    $lang['main_english']		= 'Englisch';
+    
+    // product
+    
+    $lang['main_title']		= 'Titel';
+    $lang['main_description']		= 'Beschreibung';
+    $lang['main_published']		= 'Veröffentlicht';
+    $lang['main_image']		= 'Bild';
+    $lang['main_url']		= 'url';
+    $lang['main_stock']		= 'Vorrat';
+    $lang['main_stock_available']		= 'Verfügbar';
+    $lang['main_stock_short']		= 'Läuft aus';
+    $lang['main_main_image']		= 'Hauptbild';
+    $lang['main_price_old']		= 'Preis vorher';
+    
+    
+    // admin
+    
+    $lang['main_save']		= 'Αποθήκευση';
+    $lang['main_cancel']		= 'Ακύρωση';
+    $lang['main_delete']		= 'Διαγραφή';
+    $lang['main_edit']		= 'Επεξεργασία';
+    $lang['main_add_images']		= 'Bilder Addieren';
+    
+    // navigation bar
+    
+    $lang['main_home']		= 'Haupt Seite';
+    $lang['main_catalog']		= 'Produktkatalog';
+    $lang['main_news']		= 'Nachrichten';
+    $lang['main_contact']		= 'Kontakt';
+    
+    // site info
+    
+    $lang['main_meta_description']		= '';
+    $lang['main_meta_keywords']		= '';
+    
+    $lang['main_slogan']		= '';
+    
+    // search
+    
+    $lang['main_from']		= 'Von';
+    $lang['main_to']		= 'Bis';
+    $lang['main_order_by']		= 'Sortierung';
+    $lang['main_search']		= 'Suchen';
+    
+    $lang['main_priceasc']		= 'Preis (steigend)';
+    $lang['main_pricedesc']		= 'Preis (absteigend)';
+    $lang['main_dateasc']		= 'Datum  (steigend)';
+    $lang['main_datedesc']		= 'Datum (absteigend)';
+    
+    $lang['main_page']		= 'Seite';
+    $lang['main_page_first']		= 'Erste';
+    $lang['main_page_last']		= 'Letzte';
+    $lang['main_go_back']		= 'Rückseite';
+    $lang['main_select_language']		= 'Wählen Sie Sprache';
+    $lang['main_random_product']		= 'Tip'; // trexousa prosfora
+    $lang['main_information']		= 'Information';
+    $lang['main_friendly_sites']		= 'Freundliche Seiten';
+    
+    $lang['main_price']		= 'Preis';
+    $lang['main_currency']		= '€';
+    $lang['main_price_decleration']		= '(Die Preise enthalten Mehrwertsteuer 19%)';
+    
+    // cart
+    
+    $lang['main_cart_update']		= 'Der Korb wird informiert ...';
+    $lang['main_cart_cost']		= 'Zwischensumme';
+    $lang['main_checkout']		= 'Zur Kasse';
+    $lang['main_cart']		= 'Ιhr Korb';
+    $lang['main_cart_add']		= 'In den Einkaufswagen';
+        
+    // checkout
+    
+    $lang['main_optional_data']		= 'Optional';
+    $lang['main_required_field']		= 'This field is required to continue your order';
+    $lang['main_load_data']		= 'If you have ordered in the past from us you can fill this form to load your personal data';
+    $lang['main_first_time_user']		= 'If you order for the first time please fill the following form';
+    $lang['main_phone_or_email']		= 'Phone or e-mail';
+    
+    // contact and user information
+    
+    $lang['main_full_name']		= 'Voller Name';
+    $lang['main_name']		= 'Name';
+    $lang['main_surname']		= 'Nachname';
+    $lang['main_email']		= 'Email';
+    $lang['main_birthdate']		= 'Geburtstag';
+    $lang['main_address']		= 'Addresse';
+    $lang['main_city']		= 'Stadt';
+    $lang['main_zip']		= 'Postleitzahl';
+    $lang['main_country']		= 'Land';
+    $lang['main_phone']		= 'Telefon Nummer';
+    $lang['main_company']		= 'Firma';
+    $lang['main_website']		= 'Webseite';
+    $lang['main_message']		= 'Mitteilung';
+    $lang['main_message_sent']		= 'Ihre Mitteilung wurde erfolgreich gesendet.';
+    $lang['main_message_not_sent']		= 'Ihre Mitteilung konnte nicht gesendet werden. Wir entschuldigen uns. Bitte versuchen Sie nochmal.';
+    $lang['main_submit']		= 'Abschicken';
+    $lang['main_spam_protection']		= 'Das Zweite Wählen';
+    $lang['main_spam']		= 'Das ist Spam';
+    $lang['main_not_spam']		= 'Das ist kein Spam';
+    
+    // shippment
+    
+    $lang['main_shippment']		= 'Versand';
+    $lang['main_shippment_sum']		= 'Versandkosten Summe';
+    $lang['main_shippment_express']		= 'Express';
+    $lang['main_shippment_to_door']		= 'An ihrer Tür';
+    $lang['main_shippment_cash_on_delivery']		= 'Nachname Lieferung';
+    
+    $lang['main_shippment_costs']		= '25';
+    $lang['main_shippment_express_costs']		= '13';
+    $lang['main_shippment_to_door_costs']		= '13';
+    $lang['main_shippment_cash_on_delivery_costs']		= '13';
+    
+    // costs
+    
+    $lang['main_costs_sum']		= 'Summe';
+        
+    // questionnaire
+    
+    $lang['main_questionnaire']		= 'Fragezeichen';
+    $lang['main_question1']		= 'Τί άλλο να φέρουμε';
+    $lang['main_question2']		= 'Απο που μας βρήκατε';
+    
+?>
