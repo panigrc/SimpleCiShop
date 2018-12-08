f806acabf068d09e8ee773349aaaf4f54281011e
panigrc <panigrc@x-park.gr>
2018-12-08 21:54:03 +0100

    Reformation

--- application/views/admin/catalog/list_tpl.php
+++ application/views/admin/catalog/list_tpl.php
@@ -1,4 +1,6 @@
-<p><?php echo anchor("admin/catalog/view_product/add_product", '<img src="'. base_url() .'/theme/images/add2.png" alt="Προσθήκη Προϊόντος" align="middle"> Προσθήκη Προϊόντος', 'Προσθήκη Προϊόντος'); ?></p>
+<p>
+    <?php echo anchor('admin/catalog/view_product/add_product', sprintf('<img src="%s/theme/images/add2.png" align="middle"> %s', base_url(), $this->lang->line('main_create_product'))); ?>
+</p>
 <table cellspacing=1px cellpadding=0px>
     <tr>
         <th><?php echo $this->lang->line('main_code'); ?></th>
@@ -10,20 +12,28 @@
         <th>&nbsp;</th>
         <th>&nbsp;</th>
     </tr>
-<?php foreach($products as $product): ?>
-    <tr class="<?php if(@$style === 'odd'){ echo 'even'; $style='even';} else{ echo 'odd'; $style='odd'; } ?>">
-        <td><?php echo $product['nicename']; ?></td>
-        <td><?php echo $product['title_greek']; ?></td>
-        <td><?php echo $product['category_text']; ?></td>
-        <td><?php echo date("d/m/y", $product['published']); ?></td>
-        <td>
-            <span id="stock<?php echo $product['product_id']; ?>"><?php echo $product['stock']; ?></span>
-            <a href="#" onclick="<?php echo $this->ajax->remote_function(array('update'=>'stock'.$product['product_id'], 'url'=>site_url("admin/catalog/ajax_set_stock/" . $product['product_id'] . "/". (1) ))); ?>; return FALSE;">+1</a>
-            <a href="#" onclick="<?php echo $this->ajax->remote_function(array('update'=>'stock'.$product['product_id'], 'url'=>site_url("admin/catalog/ajax_set_stock/" . $product['product_id'] . "/". (-1) ))); ?>; return FALSE;">-1</a>
-        </td>
-        <td><?php echo $product['price_greek']." / ".$product['price_german']." / ".$product['price_english']; ?></td>
-        <td><?php echo anchor("admin/catalog/view_product/edit_product/".$product['product_id'], '<img src="'. base_url() .'/theme/images/edit.png" alt="'.$this->lang->line('main_edit').'">', $this->lang->line('main_edit')); ?></td>
-        <td><?php echo anchor("admin/catalog/delete_product/".$product['product_id'], '<img src="'. base_url() .'/theme/images/delete2.png" alt="'.$this->lang->line('main_delete').'">', array('title' => $this->lang->line('main_delete'), 'onclick' => 'if( ! confirm(\'Θελετε σίγουρα να διαγραφεί η εγγραφή;\')) return FALSE;')); ?></td>
-    </tr>
-<?php endforeach; ?>
+	<?php foreach ($products as $product): ?>
+        <tr class="<?php if (@$style === 'odd') {
+			echo 'even';
+			$style = 'even';
+		} else {
+			echo 'odd';
+			$style = 'odd';
+		} ?>">
+            <td><?php echo $product['nicename']; ?></td>
+            <td><?php echo $product['title_greek']; ?></td>
+            <td><?php echo $product['category_text']; ?></td>
+            <td><?php echo date("d/m/y", $product['published']); ?></td>
+            <td>
+                <span id="stock<?php echo $product['product_id']; ?>"><?php echo $product['stock']; ?></span>
+                <a href="#"
+                   onclick="<?php echo $this->ajax->remote_function(array('update' => 'stock' . $product['product_id'], 'url' => site_url("admin/catalog/ajax_set_stock/" . $product['product_id'] . "/" . (1)))); ?>; return FALSE;">+1</a>
+                <a href="#"
+                   onclick="<?php echo $this->ajax->remote_function(array('update' => 'stock' . $product['product_id'], 'url' => site_url("admin/catalog/ajax_set_stock/" . $product['product_id'] . "/" . (-1)))); ?>; return FALSE;">-1</a>
+            </td>
+            <td><?php echo $product['price_greek'] . " / " . $product['price_german'] . " / " . $product['price_english']; ?></td>
+            <td><?php echo anchor("admin/catalog/view_product/edit_product/" . $product['product_id'], '<img src="' . base_url() . '/theme/images/edit.png" alt="' . $this->lang->line('main_edit') . '">', $this->lang->line('main_edit')); ?></td>
+            <td><?php echo anchor("admin/catalog/delete_product/" . $product['product_id'], '<img src="' . base_url() . '/theme/images/delete2.png" alt="' . $this->lang->line('main_delete') . '">', array('title' => $this->lang->line('main_delete'), 'onclick' => 'if( ! confirm(\'Θελετε σίγουρα να διαγραφεί η εγγραφή;\')) return FALSE;')); ?></td>
+        </tr>
+	<?php endforeach; ?>
 </table>
--- application/views/admin/coupon/list_tpl.php
+++ application/views/admin/coupon/list_tpl.php
@@ -1,4 +1,6 @@
-<p><?php echo anchor("coupon/view_coupon/add_coupon", '<img src="'. base_url() .'/theme/images/add2.png" alt="Προσθήκη Κουπονιών" align="middle"> Προσθήκη Κουπονιών', 'Προσθήκη Κουπονιών'); ?></p>
+<p>
+    <?php echo anchor("coupon/view_coupon/add_coupon", '<img src="' . base_url() . '/theme/images/add2.png" alt="Προσθήκη Κουπονιών" align="middle"> Προσθήκη Κουπονιών', 'Προσθήκη Κουπονιών'); ?>
+</p>
 <table cellpadding=0 cellspacing=1>
     <tr>
         <th></th>
@@ -9,15 +11,23 @@
         <th>&nbsp;</th>
         <th>&nbsp;</th>
     </tr>
-<?php foreach($coupons as $coupon): ?>
-    <tr class="<?php if(@$style === 'odd'){ echo 'even'; $style='even';} else{ echo 'odd'; $style='odd'; } ?>">
-        <td><?php echo $coupon['coupon_id']; ?></td>
-        <td><?php echo $coupon['coupon_number']; ?></td>
-        <td><?php echo date("d/m/y", $coupon['expires']); ?></td>
-        <td><?php echo $coupon['discount']; ?></td>
-        <td><?php echo $coupon['type']; ?></td>
-        <td><?php echo $coupon['used']; ?></td>
-        <td><?php echo anchor("coupon/delete_coupon/".$coupon['coupon_id'], '<img src="'. base_url() .'/theme/images/delete2.png" alt="'.$this->lang->line('main_delete').'">', array('title' => $this->lang->line('main_delete'), 'onclick' => 'if( ! confirm(\'Θελετε σίγουρα να διαγραφεί η εγγραφή;\')) return FALSE;')); ?></td>
-    </tr>
-<?php endforeach; ?>
+	<?php foreach ($coupons as $coupon): ?>
+        <tr class="<?php if (@$style === 'odd') {
+			echo 'even';
+			$style = 'even';
+		} else {
+			echo 'odd';
+			$style = 'odd';
+		} ?>">
+            <td><?php echo $coupon['coupon_id']; ?></td>
+            <td><?php echo $coupon['coupon_number']; ?></td>
+            <td><?php echo date("d/m/y", $coupon['expires']); ?></td>
+            <td><?php echo $coupon['discount']; ?></td>
+            <td><?php echo $coupon['type']; ?></td>
+            <td><?php echo $coupon['used']; ?></td>
+            <td>
+                <?php echo anchor("coupon/delete_coupon/" . $coupon['coupon_id'], '<img src="' . base_url() . '/theme/images/delete2.png" alt="' . $this->lang->line('main_delete') . '">', array('title' => $this->lang->line('main_delete'), 'onclick' => 'if( ! confirm(\'Θελετε σίγουρα να διαγραφεί η εγγραφή;\')) return FALSE;')); ?>
+            </td>
+        </tr>
+	<?php endforeach; ?>
 </table>
