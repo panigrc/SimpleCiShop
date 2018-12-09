3e03874fed2dd2defe383a567b34483199c7a9c7
panigrc <panigrc@hotmail.com>
2018-12-09 01:33:23 +0100

    fixed javascript `return FALSE` to lowercase

--- application/views/shop/cart/cart_items_tpl.php
+++ application/views/shop/cart/cart_items_tpl.php
@@ -8,7 +8,7 @@
 ?>
 					<div class="cart_item">
                         <a href="<?php echo site_url('/shop/product/index/'.$product['nicename']) ?>"><img src="<?php echo empty($product['thumb']) === FALSE ? base_url().$product['thumb'] : base_url().'images/noimage.jpg' ; ?>" class="cart_items" id="product<?php echo $i; ?>_<?php echo $product['product_id']; ?>" alt="<?php echo $product['title_'.$lang]; ?>" title="<?php echo $product['title_'.$lang]; ?>"/></a>
-                        <br /><a href="#" onclick="javascript:<?php echo $this->ajax->remote_function(array('update'=>'items','url'=>site_url('shop/cart/cart_remove'), 'hoverclass' => 'cart-active', 'loading' => "Element.show('indicator')", 'complete' => "Element.hide('indicator')", 'method' => 'post', 'with' => "'id="."product".$i."_".$product['product_id']."'")); ?>; return FALSE;"><span class="cart_remove"></span></a>
+                        <br /><a href="#" onclick="javascript:<?php echo $this->ajax->remote_function(array('update'=>'items','url'=>site_url('shop/cart/cart_remove'), 'hoverclass' => 'cart-active', 'loading' => "Element.show('indicator')", 'complete' => "Element.hide('indicator')", 'method' => 'post', 'with' => "'id="."product".$i."_".$product['product_id']."'")); ?>; return false;"><span class="cart_remove"></span></a>
 					</div>
 <?php 
             $sum += $product['price_'.$lang];
