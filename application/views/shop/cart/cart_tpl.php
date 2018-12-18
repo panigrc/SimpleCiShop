952b2ebe7afbf9e6b677545978cca9fe359c0b38
panigrc <panigrc@x-park.gr>
2018-12-18 09:15:48 +0100

    fixed checkout link

--- application/views/shop/cart/cart_tpl.php
+++ application/views/shop/cart/cart_tpl.php
@@ -2,7 +2,7 @@
 <div class="cart_top"><h2><?php echo $this->lang->line('main_cart'); ?></h2></div>
 <div id="cart" class="cart"> 
   <div id="items"></div>
-  <div><a href="<?php echo site_url("checkout"); ?>"><?php echo $this->lang->line('main_checkout'); ?></a></div>
+  <div><a href="<?php echo site_url("shop/checkout"); ?>"><?php echo $this->lang->line('main_checkout'); ?></a></div>
 </div>
 <div class="cart_bottom"></div>
 
