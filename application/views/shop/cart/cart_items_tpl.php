3863de12acc5ee12df86a86a40374143b39b94fe
Nik <panigrc@hotmail.com>
2018-12-19 17:32:56 +0100

    Replaced cart_remove.png with the respective font-awesome icon

--- application/views/shop/cart/cart_items_tpl.php
+++ application/views/shop/cart/cart_items_tpl.php
@@ -6,10 +6,17 @@
     foreach($cart as $product):
         for($i=0;$i<$product['quantity'];$i++) :
 ?>
-					<div class="cart_item">
+                    <div class="cart_item">
                         <a href="<?php echo site_url('/shop/product/index/'.$product['nicename']) ?>"><img src="<?php echo empty($product['thumb']) === FALSE ? base_url().$product['thumb'] : base_url().'images/noimage.jpg' ; ?>" class="cart_items" id="product<?php echo $i; ?>_<?php echo $product['product_id']; ?>" alt="<?php echo $product['title_'.$lang]; ?>" title="<?php echo $product['title_'.$lang]; ?>"/></a>
-                        <br /><a href="#" onclick="javascript:<?php echo $this->ajax->remote_function(array('update'=>'items','url'=>site_url('shop/cart/cart_remove'), 'hoverclass' => 'cart-active', 'loading' => "Element.show('indicator')", 'complete' => "Element.hide('indicator')", 'method' => 'post', 'with' => "'id="."product".$i."_".$product['product_id']."'")); ?>; return false;"><span class="cart_remove"></span></a>
-					</div>
+                        <br />
+                        <a href="#" onclick="javascript:<?php echo $this->ajax->remote_function(array('update'=>'items','url'=>site_url('shop/cart/cart_remove'), 'hoverclass' => 'cart-active', 'loading' => "Element.show('indicator')", 'complete' => "Element.hide('indicator')", 'method' => 'post', 'with' => "'id="."product".$i."_".$product['product_id']."'")); ?>; return false;">
+                            <span class="cart_remove"></span>
+                            <span class="fa-stack fa-2x cart-remove">
+                                <i class="fas fa-shopping-cart fa-stack-2x"></i>
+                                <i class="fas fa-arrow-up fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </div>
 <?php 
             $sum += $product['price_'.$lang];
         endfor;
