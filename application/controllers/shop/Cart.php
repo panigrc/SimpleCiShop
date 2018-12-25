8539382d0be9b33b12c5800e7cd5d034ee53932a
panigrc <panigrc@x-park.gr>
2018-12-25 17:43:52 +0100

    removed ajax

--- application/controllers/shop/Cart.php
+++ application/controllers/shop/Cart.php
@@ -6,34 +6,21 @@ class Cart extends CI_Controller {
 		parent::__construct();
 	}
 
-	public function cart_add()
+	/**
+	 * @param	$product_id
+	 */
+	public function cart_add($product_id)
 	{
-		$product_id=substr($this->input->post('id'),strpos($this->input->post('id'),'_')+1);
 		$this->cart_library->cart_add($product_id);
-		$this->cart_update();
+		redirect($this->agent->is_referral() ? $this->agent->referrer() : 'shop/catalog');
 	}
 
-	public function cart_remove()
+	/**
+	 * @param	$product_id
+	 */
+	public function cart_remove($product_id)
 	{
-		$product_id=substr($this->input->post('id'),strpos($this->input->post('id'),'_')+1);
 		$this->cart_library->cart_remove($product_id);
-		$this->cart_update();
-	}
-
-	public function cart_update()
-	{
-		/** @var array $cart contents */
-		$cart = $this->cart_library->get_cart();
-		$products = array();
-		foreach ($cart as $product => $value)
-		{
-			$products[$product] = $this->product_model->get_product($product)
-								+ $this->product_model->get_product_text($product)
-								+ $this->product_model->get_product_main_image($product)
-			;
-			$products[$product]['quantity'] = $value;
-		}
-
-		$this->load->view('shop/cart/cart_items_tpl', array('cart' => $products, 'lang' => $this->language_library->get_language()));
+		redirect($this->agent->is_referral() ? $this->agent->referrer() : 'shop/catalog');
 	}
 }
