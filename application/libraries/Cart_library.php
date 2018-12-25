079564d5a7c806c53f68d03c7317653b2da6e2ff
panigrc <panigrc@x-park.gr>
2018-12-25 17:45:42 +0100

    moved cart_block() to template_library view()

--- application/libraries/Cart_library.php
+++ application/libraries/Cart_library.php
@@ -3,31 +3,34 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Cart_library {
 
-	public function __construct($params = NULL)
-	{}
+	/**
+	 * @var	CI_Controller	$CI
+	 */
+	private $CI;
+
+	public function __construct()
+	{
+		$this->CI =& get_instance();
+	}
 
 	public function cart_add($product_id)
 	{
-		$obj =& get_instance();
-		$cart = $obj->session->userdata('cart');
-
+		$cart = $this->CI->session->userdata('cart');
 		$cart[$product_id] = (isset($cart[$product_id]))? $cart[$product_id]+1:1;
-		$obj->session->set_userdata(array('cart' => $cart));
+		$this->CI->session->set_userdata(array('cart' => $cart));
 	}
 
 	public function cart_remove($product_id)
 	{
-		$obj =& get_instance();
-		$cart = $obj->session->userdata('cart');
+		$cart = $this->CI->session->userdata('cart');
 		$cart[$product_id] = ($cart[$product_id]-1)?$cart[$product_id]-1:0;
 			if ($cart[$product_id] === 0) unset($cart[$product_id]);
-		$obj->session->set_userdata(array('cart' => $cart));
+		$this->CI->session->set_userdata(array('cart' => $cart));
 	}
 
 	public function get_cart()
 	{
-		$obj =& get_instance();
-		$cart = $obj->session->userdata('cart');
+		$cart = $this->CI->session->userdata('cart');
 		if (empty($cart))
 		{
 			return array();
@@ -38,5 +41,26 @@ class Cart_library {
 		}
 
 		return $cart;
+	}
+
+	public function cart_block()
+	{
+		/** @var array $cart contents */
+		$cart = $this->get_cart();
+		$products = array();
+		foreach ($cart as $product => $value)
+		{
+			$products[$product] = $this->CI->product_model->get_product($product)
+				+ $this->CI->product_model->get_product_text($product)
+				+ $this->CI->product_model->get_product_main_image($product)
+			;
+			$products[$product]['quantity'] = $value;
+		}
+
+		return $this->CI->load->view(
+			'shop/cart/cart_items_tpl',
+			array('cart' => $products, 'lang' => $this->CI->language_library->get_language()),
+			true
+		);
 	}
 }
