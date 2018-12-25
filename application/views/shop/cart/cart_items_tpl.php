f373b3c69154e29ccad1a35fc7e11381644d865d
panigrc <panigrc@x-park.gr>
2018-12-25 17:44:54 +0100

    removed ajax

--- application/views/shop/cart/cart_items_tpl.php
+++ application/views/shop/cart/cart_items_tpl.php
@@ -1,15 +1,15 @@
-<?php echo ( ! count($cart) OR !is_array($cart))?"":'';?>
-
-<?php 
+<?php
     
     $sum=0;
     foreach($cart as $product):
         for($i=0;$i<$product['quantity'];$i++) :
 ?>
                     <div class="cart_item">
-                        <a href="<?php echo site_url('/shop/product/index/'.$product['nicename']) ?>"><img src="<?php echo empty($product['thumb']) === FALSE ? base_url().$product['thumb'] : base_url().'images/noimage.jpg' ; ?>" class="cart_items" id="product<?php echo $i; ?>_<?php echo $product['product_id']; ?>" alt="<?php echo $product['title_'.$lang]; ?>" title="<?php echo $product['title_'.$lang]; ?>"/></a>
+                        <a href="<?php echo site_url('/shop/product/index/'.$product['nicename']) ?>">
+                            <img src="<?php echo empty($product['thumb']) === FALSE ? base_url().$product['thumb'] : base_url().'images/noimage.jpg' ; ?>" class="cart_items" id="product<?php echo $i; ?>_<?php echo $product['product_id']; ?>" alt="<?php echo $product['title_'.$lang]; ?>" title="<?php echo $product['title_'.$lang]; ?>"/>
+                        </a>
                         <br />
-                        <a href="#" onclick="javascript:<?php echo $this->ajax->remote_function(array('update'=>'items','url'=>site_url('shop/cart/cart_remove'), 'hoverclass' => 'cart-active', 'loading' => "Element.show('indicator')", 'complete' => "Element.hide('indicator')", 'method' => 'post', 'with' => "'id="."product".$i."_".$product['product_id']."'")); ?>; return false;">
+                        <a href="<?php site_url('shop/cart/cart_remove/'.$product['product_id']) ?>">
                             <span class="cart_remove"></span>
                             <span class="fa-stack fa-2x cart-remove">
                                 <i class="fas fa-shopping-cart fa-stack-2x"></i>
--- application/views/shop/cart/cart_tpl.php
+++ application/views/shop/cart/cart_tpl.php
@@ -5,17 +5,3 @@
   <div><a href="<?php echo site_url("shop/checkout"); ?>"><?php echo $this->lang->line('main_checkout'); ?></a></div>
 </div>
 <div class="cart_bottom"></div>
-
-<?php 
-echo $this->ajax->drop_receiving_element('cart',
-	 array('update'=>'items','url'=> site_url('cart/cart_add'),
-	  'accept' => 'cart_product' , 'hoverclass' => 'cart-active',
-	  'loading' => "Element.show('indicator')",
-      'complete' => "Element.hide('indicator')") );
-?>
-<script type="text/javascript"> <?php echo $this->ajax->remote_function(array('update'=>'items','url'=>site_url('cart/cart_update'))); ?>;</script>
-<div style="height: 20px; padding-top: 5px;">
-    <p id="indicator" style="display: none; margin-top: 0px; font-size: 0.6em;">
-        <i class="fas fa-spinner fa-spin"></i>
-    <?php //echo $this->lang->line('main_cart_update'); ?></p>
-</div>
