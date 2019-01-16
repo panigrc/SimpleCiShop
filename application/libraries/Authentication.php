d63c182a40551eadc8cd3579e955819b091e588d
Nik <panigrc@hotmail.com>
2019-01-16 17:59:46 +0100

    Updated the authentication library to not cause problems as it doesn't being used right now

--- application/libraries/Authentication.php
+++ application/libraries/Authentication.php
@@ -7,37 +7,32 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  */
 class Authentication {
 
-	public function __construct($params = NULL)
+	/**
+	 * @var	CI_Controller	$CI
+	 */
+	private $CI;
+
+	public function __construct()
 	{
-		$obj =& get_instance();
-		$obj->load->helper('url');
+		$this->CI =& get_instance();
+		$this->CI->load->helper('url');
 
-		$logged_in = $obj->session->userdata('logged_in');
-		if ($logged_in === 1)
-		{
-			return;
-		}
-
-		if ($obj->uri && $obj->uri->segment(1,0) != "login")
-		{
-			//redirect('login');
-			return;
-		}
+		$logged_in = $this->CI->session->userdata('logged_in');
 	}
 
 	public function authenticate($username, $password)
 	{
-		$obj =& get_instance();
-		$obj->load->helper('url');
+		$this->CI =& get_instance();
+		$this->CI->load->helper('url');
 		if ($username === "panigrc" && $password === "cool_clean_quiet")
 		{
 			//if (md5($username) === 'a13b5073a57146fb633fd061f4aaca90' && md5($password) === '446eb7e1ded9810e6e1eca2ab6575a83') {
-			$obj->session->set_userdata(array('logged_in' => '1'));
+			$this->CI->session->set_userdata(array('logged_in' => '1'));
 			return TRUE;
 		}
 		else
 		{
-			$obj->session->set_userdata(array('logged_in' => '0'));
+			$this->CI->session->set_userdata(array('logged_in' => '0'));
 			return FALSE;
 		}
 	}
