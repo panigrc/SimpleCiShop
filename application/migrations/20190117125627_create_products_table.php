cf3219c4c36f6d09dc7a3bfe924653ca1bd43657
Nik <panigrc@hotmail.com>
2019-01-17 13:29:47 +0100

    Fix migration names

--- application/migrations/20190117125627_create_products_table.php
+++ application/migrations/20190117125627_create_products_table.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_Create_categories_table extends CI_Migration
+class Migration_Create_products_table extends CI_Migration
 {
 	public function __construct()
 	{
