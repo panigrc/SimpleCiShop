440c8fd56517a968b9e41faf18edd486c1b3ef54
Nik <panigrc@hotmail.com>
2019-01-17 13:45:41 +0100

    Add latest migration

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 0;
+$config['migration_version'] = 20190117125722;
 
 /*
 |--------------------------------------------------------------------------
