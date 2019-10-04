fde08f66bf8c5ba1862836ac1b6c17c8b75b2ba2
Kridsanapong <golfitiz@gmail.com>
2019-10-04 11:37:34 +0700

    Add Thai language translation

---
+++ system/language/thai/main_lang.php
@@ -0,0 +1,137 @@
+<?php
+    $lang['main_username']		= 'ชื่อผู้่ใช้';
+    $lang['main_password']		= 'รหัสผ่าน';
+    
+    $lang['main_code']		= 'Identifier';
+    $lang['main_category']		= 'Category';
+    $lang['main_categories']		= 'Categories';
+    $lang['main_all']		= 'Όλα';
+    
+    $lang['main_language']		= 'Language';
+    $lang['main_greek']		= 'Greek';
+    $lang['main_german']		= 'German';
+    $lang['main_english']		= 'English';
+    
+    // product
+    
+    $lang['main_title']		= 'Title';
+    $lang['main_description']		= 'Description';
+    $lang['main_published']		= 'Published';
+    $lang['main_image']		= 'Image';
+    $lang['main_url']		= 'url';
+    $lang['main_stock']		= 'สต๊อก';
+    $lang['main_stock_available']		= 'Available';
+    $lang['main_stock_short']		= 'Runs out';
+    $lang['main_main_image']		= 'Main image';
+    $lang['main_price_old']		= 'ราคาเดิม';
+    
+    // admin
+    
+    $lang['main_save']		= 'Αποθήκευση';
+    $lang['main_cancel']		= 'Ακύρωση';
+    $lang['main_delete']		= 'Διαγραφή';
+    $lang['main_edit']		= 'Επεξεργασία';
+    $lang['main_add_images']		= 'Προσθήκη Εικόνων';
+    
+    // navigation bar
+    
+    $lang['main_home']		= 'หน้าหลัก';
+    $lang['main_catalog']		= 'Product Catalog';
+    $lang['main_news']		= 'ข่าว';
+    $lang['main_contact']		= 'ติดต่อ';
+    
+    // site info
+    
+    $lang['main_meta_description']		= 'Best Posters here';
+    $lang['main_meta_keywords']		= 'โปสเตอร์, ภาพยนต์, ละคร';
+    
+    $lang['main_slogan']		= '';
+    
+    // search
+    
+    $lang['main_from']		= 'จาก';
+    $lang['main_to']		= 'ถึง';
+    $lang['main_order_by']		= 'จัดเรียงโดย';
+    $lang['main_search']		= 'ค้นหา';
+    
+    $lang['main_priceasc']		= 'ราคา (น้อย)';
+    $lang['main_pricedesc']		= 'ราคา (มาก)';
+    $lang['main_dateasc']		= 'วันที่ (น้อย)';
+    $lang['main_datedesc']		= 'วันที่ (มาก)';
+    
+    $lang['main_page']		= 'หน้า';
+    $lang['main_page_first']		= 'แรก';
+    $lang['main_page_last']		= 'สุดท้าย';
+    $lang['main_go_back']		= 'กลับไป';
+    
+    $lang['main_select_language']		= 'เลือกภาษา';
+    $lang['main_random_product']		= 'คำแนะนำ';
+    $lang['main_information']		= 'Information';
+    $lang['main_friendly_sites']		= 'เว็บเพื่อนบ้าน';
+    
+    $lang['main_price']		= 'ราคา';
+    $lang['main_currency']		= 'บาท';
+    $lang['main_price_decleration']		= '(ราคารวม VAT 19%)';
+    
+    // cart
+    
+    $lang['main_cart_update']		= 'อัพเดท รถเข็น ...';
+    $lang['main_cart_cost']		= 'ยอดรวม';
+    $lang['main_checkout']		= 'สั่งซื้อ';
+    $lang['main_cart']		= 'รถเข็นของคุณ';
+    $lang['main_cart_add']		= 'เพิ่มเข้ารถเข็น';
+    
+    // checkout
+    
+    $lang['main_optional_data']		= 'ไม่บังคับ';
+    $lang['main_required_field']		= 'This field is required to continue your order';
+    $lang['main_load_data']		= 'If you have ordered in the past from us you can fill this form to load your personal data';
+    $lang['main_first_time_user']		= 'If you order for the first time please fill the following form';
+    $lang['main_phone_or_email']		= 'Phone or e-mail';
+    
+    // contact and user information
+    
+    $lang['main_full_name']		= 'ชื่อ นามสกุล';
+    $lang['main_name']		= 'ชื่อ';
+    $lang['main_surname']		= 'คำนำหน้าชื่อ';
+    $lang['main_email']		= 'อีเมล';
+    $lang['main_birthdate']		= 'วันเกิด';
+    $lang['main_address']		= 'ที่อยู่';
+    $lang['main_city']		= 'เมือง';
+    $lang['main_zip']		= 'รหัสไปรษณีย์';
+    $lang['main_country']		= 'ประเทศ';
+    $lang['main_phone']		= 'เบอร์โทรศัพท์';
+    $lang['main_company']		= 'บริษัท';
+    $lang['main_website']		= 'เว็บไซต์';
+    $lang['main_message']		= 'ข้อความ';
+    $lang['main_message_sent']		= 'Your message has been sent successfully. Thank you for contacting us. We will reply you soon.';
+    $lang['main_message_not_sent']		= 'Your message has not been sent, we apologize. Please try again.';
+    $lang['main_submit']		= 'ส่ง';
+    $lang['main_spam_protection']		= 'เลือกที่สอง';
+    $lang['main_spam']		= 'นี่คือสแปม';
+    $lang['main_not_spam']		= 'นี่ไม่ใช่สแปม';
+    
+    // shippment
+    
+    $lang['main_shippment']		= 'การขนส่ง';
+    $lang['main_shippment_sum']		= 'สรุปค่าจัดส่ง';
+    $lang['main_shippment_express']		= 'ด่วน';
+    $lang['main_shippment_to_door']		= 'ถึงหน้าประตูคุณ';
+    $lang['main_shippment_cash_on_delivery']		= 'เก็บเงินปลายทาง';
+    
+    $lang['main_shippment_costs']		= '45';
+    $lang['main_shippment_express_costs']		= '23';
+    $lang['main_shippment_to_door_costs']		= '23';
+    $lang['main_shippment_cash_on_delivery_costs']		= '23';
+        
+    // costs
+    
+    $lang['main_costs_sum']		= 'รวม';
+    
+    // questionnaire
+    
+    $lang['main_questionnaire']		= 'แบบสอบถาม';
+    $lang['main_question1']		= 'Τί άλλο να φέρουμε';
+    $lang['main_question2']		= 'Απο που μας βρήκατε';
+    
+?>
