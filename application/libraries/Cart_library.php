9cd97eeaf7a3aa372fd66e25a823f70903e4e1ea
panigrc <panigrc@x-park.gr>
2019-11-18 22:07:23 +0100

    Fix un-initialized cart

--- application/libraries/Cart_library.php
+++ application/libraries/Cart_library.php
@@ -18,7 +18,7 @@ class Cart_library {
 	 */
 	public function cart_add($product_id)
 	{
-		$cart = $this->CI->session->userdata('cart');
+		$cart = $this->initialize_cart();
 
 		$cart['products'][$product_id] = (isset($cart['products'][$product_id])) ? $cart['products'][$product_id] + 1 : 1;
 
@@ -30,7 +30,7 @@ class Cart_library {
 	 */
 	public function cart_remove($product_id)
 	{
-		$cart = $this->CI->session->userdata('cart');
+		$cart = $this->initialize_cart();
 
 		if (! isset($cart['products'][$product_id])) {
 			return;
@@ -50,17 +50,27 @@ class Cart_library {
 	 */
 	public function get_cart()
 	{
-		$cart = $this->CI->session->userdata('cart');
-
-		if (empty($cart))
-		{
-			return [];
-		}
+		$cart = $this->initialize_cart();
 
 		if (isset($cart['affiliate']))
 		{
 			unset($cart['affiliate']);
 		}
+
+		return $cart;
+	}
+
+	/**
+	 * @return	array
+	 * @todo	cart should be converted to a class
+	 */
+	protected function initialize_cart()
+	{
+		$cart = $this->CI->session->userdata('cart');
+
+		$cart['products'] = $cart['products'] ?? [];
+
+		$this->CI->session->set_userdata(['cart' => $cart]);
 
 		return $cart;
 	}
