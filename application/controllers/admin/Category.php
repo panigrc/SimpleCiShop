84d5034aeebb88bacbd00f6227cb364802c766be
panigrc <panigrc@x-park.gr>
2020-05-02 16:05:59 +0200

    Replace input data with parameters in Category Model
    ### Added
    
    - Method parameters.
    - Strict types to method parameters in Category_model.
    - Strict return types to methods in Category_model.
    
    ### Changed
    
    - `Category::add_category()` uses the updated `Category_model::add_category()` method
    - `Category::set_category()` uses the updated `Category_model::set_category()` method
    - Arrays to short type.
    
    ### Removed
    
    - Direct post input (`$this->input->post()`).
    - Unnecessary docblock.
    - Unnecessary `Category_model` constructor.
    - Commented code.

--- application/controllers/admin/Category.php
+++ application/controllers/admin/Category.php
@@ -61,13 +61,54 @@ class Category extends CI_Controller {
 
 	public function set_category()
 	{
-		$this->category_model->set_category();
+		$this->category_model->set_category(
+			$this->input->post('category_id'),
+			$this->input->post('slug'),
+			$this->input->post('parent_category_id')
+		);
+		$this->category_model->set_category_text(
+			$this->input->post('category_text_id_greek'),
+			$this->input->post('category_name_greek'),
+			$this->input->post('category_description_greek')
+		);
+		$this->category_model->set_category_text(
+			$this->input->post('category_text_id_german'),
+			$this->input->post('category_name_german'),
+			$this->input->post('category_description_german')
+		);
+		$this->category_model->set_category_text(
+			$this->input->post('category_text_id_english'),
+			$this->input->post('category_name_english'),
+			$this->input->post('category_description_english')
+		);
 		redirect('admin/category');
 	}
 
 	public function add_category()
 	{
-		$this->category_model->add_category();
+		$category_id = $this->category_model->add_category(
+			$this->input->post('slug'),
+			$this->input->post('parent_category_id')
+		);
+		$this->category_model->add_category_text(
+			$category_id,
+			'greek',
+			$this->input->post('category_name_greek'),
+			$this->input->post('category_description_greek')
+		);
+		$this->category_model->add_category_text(
+			$category_id,
+			'german',
+			$this->input->post('category_name_german'),
+			$this->input->post('category_description_german')
+		);
+		$this->category_model->add_category_text(
+			$category_id,
+			'english',
+			$this->input->post('category_name_english'),
+			$this->input->post('category_description_english')
+		);
+
 		redirect('admin/category');
 	}
 }
